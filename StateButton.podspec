2e2e95b717e9d642a46c95e6543e609a7dcb2652
fu-zhiyang <fuzhiyang0123@gmail.com>
2015-06-26 14:13:26 +0900

    init commit

---
+++ StateButton.podspec
@@ -0,0 +1,30 @@
+Pod::Spec.new do |s|
+ 
+  s.platform = :ios
+  s.ios.deployment_target = '8.0'
+  s.name = "StateButton"
+  s.summary = "Button with different states"
+  s.requires_arc = true
+ 
+
+  s.version = "0.1.0"
+ 
+
+  s.license = { :type => "MIT", :file => "LICENSE" }
+ 
+  
+  s.author = { "zyfu000" => "fuzhiyang0123@gmail.com" }
+ 
+ 
+  s.homepage = "https://github.com/zyfu0000/StateButton"
+ 
+ 
+  s.source = { :git => "https://github.com/zyfu0000/StateButton", :tag => "#{s.version}"}
+ 
+
+  s.framework = "UIKit"
+ 
+
+  s.source_files = "StateButton/*.{swift}"
+ 
+end
---
+++ StateButtonDemo/StateButtonDemo/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  StateButtonDemo
+//
+//  Created by fu-zhiyang on 6/26/15.
+//  Copyright (c) 2015 fu-zhiyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var stateButton : StateButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        stateButton.titles = ["facebook"]
+        stateButton.images = [UIImage(named: "icon_login_facebook")!, UIImage(named: "icon_login_twitter")!, UIImage(named: "icon_login_mail")!]
+        stateButton.titleColors = [UIColor.redColor(), UIColor.greenColor(), UIColor.blueColor()]
+        stateButton.closures = [ { (index) -> () in
+                println("State \(index)")
+            },
+            { (index) -> () in
+                println("State \(index)")
+            },
+            { (index) -> () in
+                println("State \(index)")
+            }]
+        stateButton.transitions = [(kCATransitionPush, kCATransitionFromTop), (kCATransitionMoveIn, kCATransitionFromLeft), (kCATransitionReveal, kCATransitionFromRight)]
+    }
+    
+}
+
